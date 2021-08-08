cff4fd94c7c6f7556f737ff789afe8c0148e5a53
CutyDog <silver.soul.818@gmail.com>
2021-08-09 01:53:18 +0900

    Bootstrap, User page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,12 @@ class UsersController < ApplicationController
     end
 
     def show
+        @user = User.find(params[:id])
+        @news_categories = []
+        categories = [:domestic, :world, :business, :entertainment, :sports, :it, :science]
+        categories.each do |category|
+            @news_categories << category if @user.favorites.first[category]==true
+        end
     end
 
     def create
